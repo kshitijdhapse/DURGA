31b6611f1e229a88704620f1318aa9a0c96e6e29
kshitijdhapse <kshitijdhapse@gmail.com>
2024-08-07 22:43:07 +0530

    categories dropdown

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -7,7 +7,22 @@ class FoodItem(models.Model):
     image = models.ImageField(blank=True,null=True, upload_to='images/')
     description = models.CharField("Description", max_length=200)
     price = models.DecimalField("Price", max_digits=10, decimal_places=2)
-    category = models.CharField("Category", max_length=50)
+    
+    foodchoices=(
+        ('Breakfast','Breakfast'),
+        ('Sandwich','Sandwich'),
+        ('Misal Pav','Misal Pav'),
+        ('Dosa','Dosa'),
+        ('Pav Bhaji','Pav Bhaji'),
+        ('Bhurji','Bhurji'),
+        ('Cold Coffee','Cold Coffee'),
+        ('Shakes','Shakes'),
+        ('Pizza','Pizza'),
+        ('Cold Drink','Cold Drink'),
+        ('Ice Cream','Ice Cream'),
+        ('Mastani','Mastani'),
+        )
+    category = models.CharField("Category",choices=foodchoices, max_length=50)
     hide = models.BooleanField("Hide", default=False)
     topping = models.CharField("Topping", max_length=50, blank=True, null=True)
     topping_price = models.DecimalField("Topping Price", max_digits=10, decimal_places=2, blank=True, null=True)
